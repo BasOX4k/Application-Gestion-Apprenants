effab6afe06818a62af463dee52c83c12ba9ab83
BasOX4k <adodo.timotheec@gmail.com>
2024-04-08 16:57:01 +0200

    début de l'architecture du site
    Ajout de plusieurs fichier pour la mise en place du site

---
+++ src/Models/Utilisateur.php
@@ -0,0 +1,116 @@
+<?php
+
+class Utilisateur
+{
+    private $id;
+    private $nom;
+    private $prenom;
+    private $email;
+    private $mdp;
+    
+    
+    
+
+
+    function __construct($nom, $prenom, $email, $mdp, $id = null)
+    {
+        $this->id = $id;
+        $this->nom = $nom;
+        $this->prenom = $prenom;
+        $this->email = $email;
+        $this->mdp = $mdp;
+        
+    }
+
+    /**
+     * Get the value of id
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set the value of id
+     */
+    public function setId($id): self
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of nom
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * Set the value of nom
+     */
+    public function setNom($nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of prenom
+     */
+    public function getPrenom()
+    {
+        return $this->prenom;
+    }
+
+    /**
+     * Set the value of prenom
+     */
+    public function setPrenom($prenom): self
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of email
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set the value of email
+     */
+    public function setEmail($email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of mdp
+     */
+    public function getMdp()
+    {
+        return $this->mdp;
+    }
+
+    /**
+     * Set the value of mdp
+     */
+    public function setMdp($mdp): self
+    {
+        $this->mdp = $mdp;
+
+        return $this;
+    }
+}
+
+   
